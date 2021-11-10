750589111f3cc6f2706227f893790bd448595253
Krzysztof Pawełczak <lbkrpaw87@gmail.com>
2021-11-10 12:39:28 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StoryblokService } from './storyblok.service';
+import { Components } from './components';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'spartacus-storyblock';
+export class AppComponent implements OnInit {
+  story = { content: null, name: '' };
+  components = Components;
+
+  constructor(private storyblokService: StoryblokService) {
+    window.storyblok.init();
+    window.storyblok.on(['change', 'published'], function () {
+      location.reload(true)
+    });
+  }
+
+  ngOnInit() {
+    this.storyblokService.getStory('home', { version: 'draft' })
+      .then(data => this.story = data.story);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { DynamicModule } from 'ng-dynamic-component';
 import { AppComponent } from './app.component';
+import { StoryblokService } from './storyblok.service';
+import { StoryblokDirective } from './storyblok.directive';
+import { TeaserComponent } from './teaser/teaser.component';
+import { GridComponent } from './grid/grid.component';
+import { PageComponent } from './page/page.component';
+import { FeatureComponent } from './feature/feature.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    StoryblokDirective,
+    TeaserComponent,
+    GridComponent,
+    FeatureComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DynamicModule
+    // DynamicModule.withComponents([
+    //   PageComponent,
+    //   TeaserComponent,
+    //   GridComponent,
+    //   FeatureComponent
+    // ])
   ],
-  providers: [],
+  providers: [
+    StoryblokService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
